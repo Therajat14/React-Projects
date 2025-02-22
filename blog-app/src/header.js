261e5668265a1b9d0e9706088f7dbdc5d4c6841d
therajat14 <heylegend14@gmail.com>
2025-02-23 01:14:36 +0530

    Updated header and styles, added search functionality, and modified cache files

--- blog-app/src/header.js
+++ blog-app/src/header.js
@@ -1,9 +1,11 @@
 import Navbar from "./nav"
+import Search from "./search"
 export default function Header() {
     return (
         <header>
             <h1>  Blog App </h1>
             <Navbar />
+            <Search />
         </header>
     )
 }
---
+++ blog-app/src/search.js
@@ -0,0 +1,11 @@
+const Search = () => {
+    return (
+        <form >
+            <label htmlFor="SearchBOx">
+                <input type="text" placeholder="Search" />
+            </label>
+        </form>
+    )
+}
+
+export default Search;
