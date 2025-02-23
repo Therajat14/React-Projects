52329cb9de3605eb51eef246ba5aa8e9cb292a77
therajat14 <heylegend14@gmail.com>
2025-02-24 01:43:37 +0530

    added close button to footer

--- blog-app/src/footer.js
+++ blog-app/src/footer.js
@@ -1,14 +1,27 @@
+import { useState } from "react";
 import React from "react";
 import "./footer.css";
+import { FaRegWindowClose } from "react-icons/fa";
 
 export default function Footer() {
+    const [showFooter, setShowFooter] = useState(true);
     return (
-        <footer className="footer">
-            <p>© {new Date().getFullYear()} BlogApp. All rights reserved.</p>
-            <p>
-                Built with 💙 by <a href="https://github.com/Therajat14" target="_blank" rel="noopener noreferrer">Rajat Singh</a>
-            </p>
-            <p className="footer-note">🚧 This blog app is still under development. Expect updates soon! 🚀</p>
-        </footer>
+        <>
+            {
+                showFooter &&
+                <footer className="footer">
+                    <div className="closeButton"
+                        onClick={() => setShowFooter(false)}>
+                        <FaRegWindowClose />
+                    </div>
+                    <p>© {new Date().getFullYear()} BlogApp. All rights reserved.</p>
+                    <p>
+                        Built with 💙 by <a href="https://github.com/Therajat14" target="_blank" rel="noopener noreferrer">Rajat Singh</a>
+                    </p>
+                    <p className="footer-note">🚧 This blog app is still under development. Expect updates soon! 🚀</p>
+                </footer>
+            }
+        </>
+
     );
 }
