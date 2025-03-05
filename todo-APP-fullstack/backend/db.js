8c49cfae166980ba43b3890bcde32fd1f7a52e24
therajat14 <heylegend14@gmail.com>
2025-03-05 22:17:26 +0530

    Schema eroor fixed

--- todo-APP-fullstack/backend/db.js
+++ todo-APP-fullstack/backend/db.js
@@ -1,7 +1,24 @@
 import mongoose from "mongoose";
+import dotEnv from 'dotenv';
 
-const URI = "mongodb+srv://therajat14:therajat14@therajat14.yma6r.mongodb.net/todoApp";
+dotEnv.config();
+const URI = process.env.MONGO_URI;
+console.log(URI);
 
-mongoose.connect(URI, { useNewUrlParser: true, useUnifiedTopology: true })
+
+mongoose.connect(URI)
     .then(() => console.log("Connected to MongoDB"))
     .catch(err => console.error("Connection failed", err));
+
+const toDoSchema = new mongoose.Schema({
+    title: { type: String, required: true },
+    description: { type: String, required: true }, // Fixed spelling: "description" description
+    isCompleted: { type: Boolean, default: false },
+});
+
+
+
+
+
+const todo = mongoose.model('todos', toDoSchema);
+export default todo;
