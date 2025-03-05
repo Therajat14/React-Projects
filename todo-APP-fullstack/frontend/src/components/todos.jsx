4041a3d76b86c64f42c713151b27ae54f37434f3
therajat14 <heylegend14@gmail.com>
2025-03-06 02:41:21 +0530

    Implemented Todos component to display and manage tasks

--- todo-APP-fullstack/frontend/src/components/todos.jsx
+++ todo-APP-fullstack/frontend/src/components/todos.jsx
@@ -1,12 +1,21 @@
+import { useContext } from "react";
+import TodoProvider, { TodoContext } from "../todoContext";
+
 const Todos = () => {
+    const { todos } = useContext(TodoContext);
     return (
         <div>
             <ul>
                 {
                     todos.map(todo => <li>
-                        <h2>todo.title</h2>
-                        <p>todo.description</p>
-                        <input type="checkbox" checked={todo.isCompleted} />
+                        <div className="todoHead">
+                            <h2>{todo.title}</h2>
+                            <input type="checkbox" checked={todo.isCompleted} />
+
+                        </div>
+
+                        <p>{todo.description}</p>
+
                     </li>)
                 }
             </ul>
