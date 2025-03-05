7c0572b6b129793209262c344be91ae9de5c3803
therajat14 <heylegend14@gmail.com>
2025-03-06 02:41:21 +0530

    Initialized App component for structuring the React app

--- todo-APP-fullstack/frontend/src/App.jsx
+++ todo-APP-fullstack/frontend/src/App.jsx
@@ -1,17 +1,37 @@
 
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { CreateTodo } from './components/createTodo'
 import TodoProvider from './todoContext';
+import Todos from './components/todos';
 
 
 
 function App() {
 
+  // useEffect(() => {
+  //   console.log("Fetching data...");
+
+  //   const fetchData = async () => {
+  //     try {
+  //       const response = await fetch("http://localhost:9000"); // ✅ Fixed URL
+  //       if (!response.ok) {
+  //         throw new Error(`HTTP error! Status: ${response.status}`);
+  //       }
+  //       const data = await response.json(); // ✅ Corrected JSON parsing
+  //       console.log(data);
+  //     } catch (error) {
+  //       console.error("Error fetching data:", error);
+  //     }
+  //   };
+
+  //   fetchData();
+  // }, []);
 
   return (
     <>
       <TodoProvider>
         <CreateTodo />
+        <Todos />
       </TodoProvider>
     </>
   )
