e1f1cc628c6446233f54f5b6694eaa7a93209830
therajat14 <heylegend14@gmail.com>
2025-03-07 14:14:22 +0530

    Improved createTodo component with better state handling

--- todo-APP-fullstack/frontend/src/components/createTodo.jsx
+++ todo-APP-fullstack/frontend/src/components/createTodo.jsx
@@ -1,19 +1,40 @@
+import { useContext, useState } from "react";
+import { TodoContext } from "../todoContext";
 
 export function CreateTodo() {
-    return (<form
-        onSubmit={
-            (e) => { e.preventDefault() }
-        }
-    >
-        <h1>Add  a new Todo</h1>
-        <div className="inputCompo">
-            <label htmlFor="name"></label>
-            <input type="text" id="name" name="name" placeholder="Title" />
-            <label htmlFor="description"></label>
-            <input type="textarea" id="description" name="description" placeholder="Description" className="textarea" />
+    const { todos, setTodos } = useContext(TodoContext);
+    const [title, setTitle] = useState("");
+    const [description, setDescription] = useState("");
 
-        </div>
-        <button type="submit">Add</button>
 
-    </form>)
+
+    return (
+        <form >
+            <h1>Add a New Todo</h1>
+            <div className="inputCompo">
+                <label htmlFor="title">Title:</label>
+                <input
+                    type="text"
+                    id="title"
+                    name="title"
+                    placeholder="Title"
+                    value={title}
+                    onChange={(e) => setTitle(e.target.value)}
+                    required
+                />
+
+                <label htmlFor="description">Description:</label>
+                <textarea
+                    id="description"
+                    name="description"
+                    placeholder="Description"
+                    className="textarea"
+                    value={description}
+                    onChange={(e) => setDescription(e.target.value)}
+                    required
+                ></textarea>
+            </div>
+            <button type="submit">Add</button>
+        </form>
+    );
 }
