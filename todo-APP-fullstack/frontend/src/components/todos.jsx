d24eccb4fc73346c4c71aa9dfadd551a6a786361
therajat14 <heylegend14@gmail.com>
2025-03-08 01:32:12 +0530

    Refactored todos component for better performance

--- todo-APP-fullstack/frontend/src/components/todos.jsx
+++ todo-APP-fullstack/frontend/src/components/todos.jsx
@@ -1,11 +1,43 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { TodoContext } from "../todoContext";
 
 const Todos = () => {
-    const { todosItem } = useContext(TodoContext);
+    const { todosItem, setTodos } = useContext(TodoContext);
+
+
+    const markAsCompleted = async (todoId) => {
+        try {
+            const response = await fetch("http://localhost:9000/completed", {
+                method: "PUT", // ✅ Use PUT for updates
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify({ _id: todoId }) // ✅ Sending todo._id in body
+            });
+
+
+            if (!response.ok) {
+                throw new Error(`HTTP error! Status: ${response.status}`);
+            }
+
+            // setTodo(todosItem.map(todo =>
+            //     todo._id === todoId ? { ...todo, isCompleted: !todo.isCompleted } : todo
+            // ));
+            setTodos(todosItem.map(todo =>
+                todo._id === todoId ? { ...todo, isCompleted: !todo.isCompleted } : todo
+            ));
+
+
+            const data = await response.json();
+            console.log("Todo Updated:", data);
+
+        } catch (error) {
+            console.error("Error updating todo:", error);
+        }
+
+    };
 
     return (
-        <div>
+        <div className="todos">
+            {todosItem.length < 1 && <p>No ToDO item  Present </p>}
             <ul>
                 {todosItem.map((todo, index) => (
                     <li key={todo._id || index}>
@@ -13,10 +45,17 @@ const Todos = () => {
                             <label htmlFor="check">
                                 <h2>{todo.title}</h2>
                             </label>
-                            <input type="checkbox" id="check" checked={todo.isCompleted} readOnly />
-                        </div>
+                            <input type="checkbox" id="check" checked={todo.isCompleted} onChange={() => {
+                                markAsCompleted(todo._id)
 
-                        <p>{todo.description}</p>
+                            }}
+                            />
+                        </div>
+                        <div className="todoDetails">
+                            <p>{todo.description}</p>
+                            {/* <p className="todo-id"> id: {todo._id}</p> */}
+                        </div>
+                        <p className="todo-id"> id: {todo._id}</p>
                     </li>
                 ))}
             </ul>
