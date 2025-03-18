34483acc70cb59af15bf986e305df080246c7594
therajat14 <heylegend14@gmail.com>
2025-03-18 12:02:39 +0530

    lernt display in tailwind

--- todo-APP-fullstack/frontend/src/App.jsx
+++ todo-APP-fullstack/frontend/src/App.jsx
@@ -3,9 +3,15 @@ import { CreateTodo } from "./components/createTodo";
 import TodoProvider from "./todoContext";
 import Todos from "./components/todos";
 import Header from "./components/header";
+import 'dotenv/config'
+dotenv.config();
+
 
 
 function App() {
+  console.log(process.env.REACT_APP_BACKEND_URL);
+  console.log(process.env.REACT_APP_API_KEY);
+
   return (
     <BrowserRouter>
       <Header />
